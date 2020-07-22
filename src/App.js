9d3cd8ff4c1526b79d50d37747a9d119c6d2df5a
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2020-07-22 02:08:07 +0200

    adding the modal for userName input

--- src/App.js
+++ src/App.js
@@ -11,14 +11,48 @@ import FlipMove from 'react-flip-move'
 
 import logo from './assets/messenger-logo.png'
 
+import Modal from '@material-ui/core/Modal'
+import { makeStyles } from '@material-ui/core/styles'
+import { Button, Input } from '@material-ui/core';
+
+const getModalStyle = () => {
+  const top = 45;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-50%, -50%)`,
+    textAlign: 'center',
+    border: 'none',
+    outline: 'none'
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    maxWidth: '100%',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(3, 3, 3),
+  }
+}));
+
 const App = () => {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle)
+
   const [input, setInput] = useState('');
   const [messeges, setMesseges] = useState([]);
   const [userName, setUsername] = useState('');
+  const [userInput, setUserInput] = useState('')
+
+  const [open, setOpen] = useState(true)
 
   useEffect(() => {
-    setUsername(prompt('Please Enter Your Username'))
-
     db.collection('messeges').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       setMesseges(snapshot.docs.map(doc => {
         return {
@@ -45,17 +79,41 @@ const App = () => {
 
   return (
     <div className="App">
+      <Modal
+        open={open}
+        onClose={open}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form style={{display: 'flex', flexDirection: 'column' }} onSubmit={() => {
+            setUsername(userInput)
+            setUserInput('')
+            setOpen(false)
+          } }>
+            <Input 
+              required
+              type='text'
+              color='primary'
+              placeholder='Enter Your Name'
+              value={userInput}
+              onChange={e => setUserInput(e.target.value)}
+              className='input'
+            />
+            <Button type='submit'>Confirm</Button>
+          </form>
+        </div>
+      </Modal>
       <img src={logo} alt='messenger clone logo' width='150' />
       <h1>Messenger Clone</h1>
       <h3>Welcome {userName} </h3>
-      <FlipMove>
-        {
-          messeges.map(messege => {
-            return <MessegeItem messege={messege} MyUserName={userName} key={messege.id} />
-          })
-        }
-      </FlipMove>
-
+      { userName &&
+        <FlipMove>
+          {
+            messeges.map(messege => {
+              return <MessegeItem messege={messege} MyUserName={userName} key={messege.id} />
+            })
+          }
+        </FlipMove>
+      }
       <FormInput 
         messegesHandler={messegesHandler}
         inputHandler={inputHandler}
